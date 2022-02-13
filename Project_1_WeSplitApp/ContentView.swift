ac26a28ddd4160e1de5c7531456b1f5e4d225656
IgorChumikov <chumikov.igor@mail.ru>
2022-02-13 20:32:31 +0300

    add

---
+++ Project_1_WeSplitApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project_1_WeSplitApp
+//
+//  Created by Игорь Чумиков on 13.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project_1_WeSplitApp/Project_1_WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1_WeSplitAppApp.swift
+//  Project_1_WeSplitApp
+//
+//  Created by Игорь Чумиков on 13.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_1_WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
